87c49b700b165428b4b45ae6c0545ddf142f6bdd
Nikunj Mehra <nikunjmehra935@gmail.com>
2024-01-31 16:13:53 +0530

    Final touches

--- components/media_item.tsx
+++ components/media_item.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 
 import useLoadImage from "@/hooks/useLoadImage";
+import usePlayer from "@/hooks/usePlayer";
+
 import { Song } from "@/types";
 
 interface MediaItemProps {
@@ -11,6 +13,7 @@ interface MediaItemProps {
 }
 
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+  const player = usePlayer();
   const imageUrl = useLoadImage(data);
 
   const handleClick = () => {
@@ -18,7 +21,7 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
       return onClick(data.id);
     }
 
-    // TODO: Default turn on player
+    return player.setId(data.id);
   };
 
   return (
